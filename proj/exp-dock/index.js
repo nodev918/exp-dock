5040ad42990bccc490f241b59f70c69ea46342ef
nodev918 <devnode77@gmail.com>
2021-11-21 12:31:27 +0800

    hh

---
+++ proj/exp-dock/index.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const app = express()
+
+app.get('/',(req,res)=>{
+    res.send("hello world")
+})
+
+const PORT = 3000
+app.listen(PORT,()=>{
+    console.log(`serv: ${PORT}`)
+})
